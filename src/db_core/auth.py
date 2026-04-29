64e2fa9eaa7b4c2bd3b4da8d9aeab2b36ea0dabc
mvishnuprasad1999 <vishnuprasad007007@gmail.com>
2026-04-30 01:42:44 +0530

    fixed hash password,email

--- src/db_core/auth.py
+++ src/db_core/auth.py
@@ -12,12 +12,24 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # Hash
-def hash_password(password: str):
-    return pwd_context.hash(password)
+# def hash_password(password: str):
+#     return pwd_context.hash(password)
 
-# Verify
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+# # Verify
+# def verify_password(plain, hashed):
+#     return pwd_context.verify(plain, hashed)
+import bcrypt
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
+    )
 
 # Token
 def create_access_token(data: dict):
